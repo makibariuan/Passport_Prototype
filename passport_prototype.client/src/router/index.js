ac6ed2a8d61b642613aaefd414da4f9daef2c806
JohnMark Guillero <johnmarkguillero1@gmail.com>
2026-03-26 14:43:29 +0800

    PUshing for UserAdjudication

--- passport_prototype.client/src/router/index.js
+++ passport_prototype.client/src/router/index.js
@@ -16,6 +16,7 @@ import ManageCitizenPage from "@/components/ManageCitizenPage.vue";
 import AdjudicationDetailsPage from "@/components/HR/AdjudicationDetailsPage.vue";
 import BiometricTest from "@/components/BiometricTest.vue";
 import AttendanceManagement from "@/components/HR/AttendanceManagement.vue";
+import IDDetails from "@/components/Employee/IDDetails.vue";
 import CreateApplication from "@/components/CreateApplication.vue";
 import ApplicationAssessment from "@/components/ApplicationAssessment.vue";
 import ApplicationHistory from "@/components/ApplicationHistory.vue";
@@ -80,13 +81,7 @@ const routes = [
   { path: "/profile", name: "PersonalDataSheet", component: PersonalDataSheet },
   { path: "/persons-profile", name: "PersonsDataSheet", component: PersonsDataSheet },
   { path: "/application/new", name: "Application", component: CreateApplication },
-  { path: "/application/history", name: "ApplicationHistory", component: ApplicationHistory },
-  { path: "/privacy-policy", name: "PrivacyPolicy", component: PrivacyPolicy },
-  {
-    path: "/application/manage",
-    name: "ManageExistingApplicationHistory",
-    component: ManageExistingApplication,
-  },
+  { path: "/id-details", name: "IDDetails", component: IDDetails },
 
   { path: "/id-details", name: "IDDetails", component: IDDetails },
 
